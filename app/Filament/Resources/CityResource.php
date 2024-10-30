da95eda6a6dfc5f22a7ffc4ca1af522184f82892
mehedi1256 <mehedihassanshovospirit@gmail.com>
2024-10-30 12:43:19 +0600

    updated

---
+++ app/Filament/Resources/CityResource.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CityResource\Pages;
+use App\Filament\Resources\CityResource\RelationManagers;
+use App\Models\City;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CityResource extends Resource
+{
+    protected static ?string $model = City::class;
+
+    // for custom icon
+    protected static ?string $navigationIcon = 'heroicon-o-flag';
+
+    // for custom navigation label
+    protected static ?string $navigationLabel = 'City';
+
+    // for custom model label in view
+    protected static ?string $modelLabel = 'City';
+
+    // for custom grouping navigation
+    protected static ?string $navigationGroup = 'System Management';
+
+    // for custom url generator
+    protected static ?string $slug = 'city';
+
+    // for sort navigation menu
+    protected static ?int $navigationSort = 3;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('state_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('state_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCities::route('/'),
+            'create' => Pages\CreateCity::route('/create'),
+            'view' => Pages\ViewCity::route('/{record}'),
+            'edit' => Pages\EditCity::route('/{record}/edit'),
+        ];
+    }
+}
